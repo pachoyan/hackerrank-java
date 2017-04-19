09ac0d283508dc55673ce68bdc628977e920d338
pachoyan <pchorro@gfi.es>
2017-04-19 10:35:42 +0200

    Add challenges

---
+++ src/main/java/introduction/HelloWorld.java
@@ -0,0 +1,13 @@
+//Java Hello World - Challenge
+
+package introduction;
+
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+
+}
---
+++ src/main/java/introduction/JavaStdinAndStdout1.java
@@ -0,0 +1,20 @@
+//Java Stdin and Stdout I - Challenge
+
+package introduction;
+
+import java.util.Scanner;
+
+public class JavaStdinAndStdout1 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+
+}
