937d5d66aff16b3d4631959df2df3124cf71a86f
pachoyan <pchorro@gfi.es>
2017-04-21 14:14:05 +0200

    Add challenge Java End Of File

--- src/main/java/introduction/JavaEndOfFile.java
+++ src/main/java/introduction/JavaEndOfFile.java
@@ -3,5 +3,17 @@
 package introduction;
 
 
+import java.util.Scanner;
+
 public class JavaEndOfFile {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int cont = 1;
+        while (sc.hasNext()) {
+            String text = sc.nextLine();
+            System.out.println(cont + " " + text);
+            cont++;
+        }
+    }
 }
