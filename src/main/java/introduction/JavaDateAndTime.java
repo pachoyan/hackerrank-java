baa545780313f14c2293df7253e8bbbc53d502b7
pachoyan <pchorro@gfi.es>
2017-04-21 14:26:46 +0200

    Add challenge to do it

---
+++ src/main/java/introduction/JavaDateAndTime.java
@@ -0,0 +1,17 @@
+//Java Date and Time - Challenge
+
+package introduction;
+
+
+import java.util.Scanner;
+
+public class JavaDateAndTime {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String month = in.next();
+        String day = in.next();
+        String year = in.next();
+    }
+
+}
