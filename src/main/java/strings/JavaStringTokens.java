e9e9a17748393482e81f61fa2b35c5229ffa6d50
pachoyan <pauchorroyanguas@gmail.com>
2017-04-23 23:52:44 +0200

    Add string chalenges

---
+++ src/main/java/strings/JavaStringTokens.java
@@ -0,0 +1,16 @@
+//Java String Tokens - Challenge
+
+package strings;
+
+
+import java.util.Scanner;
+
+public class JavaStringTokens {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        // Write your code here.
+        scan.close();
+    }
+}
